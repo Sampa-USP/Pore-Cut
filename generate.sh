e108101d968d6cd8dbfa09a6c132ac9523dde0d8
João <joaocassianox7x@gmail.com>
2022-10-07 13:26:24 -0300

    update

--- generate.sh
+++ generate.sh
@@ -4,6 +4,7 @@ for i in 5 7.5 10 12.5 15 17.5 20;do
     python cut_pore.py files/sio2.xyz $i -o pore_${i}_silanol_5.pdb --option plateh --silanol-density 5
     python cut_pore.py files/sio2.xyz $i -o pore_${i}_silanol_5.xyz --option plateh --silanol-density 5
     python rename_pdb_plate.py pore_${i}_silanol_5.pdb ${i} --detect-ionized-o --rename-label
+    grep -v 'CONECT' rpore_${i}_silanol_5.pdb > pore_${i}_silanol_5.pdb
 done
 
 mv *.xyz output/
